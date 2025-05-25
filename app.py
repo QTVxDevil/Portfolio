929889bac807adcd48757e1fa170cb0ba9bd3838
QTVxDevil <quyettv1302@gmail.com>
2025-05-25 19:22:17 +0700

    portfolio

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template
 import os
-
 from config import SECRET_KEY
 
 app = Flask(
     __name__,
-    template_folder=os.path.abspath(os.path.join(os.path.dirname(__file__), '../frontend/templates')),
-    static_folder=os.path.abspath(os.path.join(os.path.dirname(__file__), '../frontend/static'))
+    template_folder=os.path.abspath('templates'),
+    static_folder=os.path.abspath('static')
 )
 app.secret_key = SECRET_KEY
 
@@ -15,4 +14,4 @@ def home():
     return render_template('base.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
