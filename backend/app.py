2634a98f98ca0a9f258c9bdc2dcb4422ea2b2169
QTVxDevil <quyettv1302@gmail.com>
2025-05-25 16:12:44 +0700

    backend

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
 import os
 
+from config import SECRET_KEY
+
 app = Flask(
     __name__,
     template_folder=os.path.abspath(os.path.join(os.path.dirname(__file__), '../frontend/templates')),
     static_folder=os.path.abspath(os.path.join(os.path.dirname(__file__), '../frontend/static'))
 )
+app.secret_key = SECRET_KEY
 
 @app.route('/')
 def home():
